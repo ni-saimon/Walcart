e2e9f6b54540b0b4e4e5c48617470e5e964338b4
Nafiz Imtiaz Saimon <40791066+ni-saimon@users.noreply.github.com>
2022-09-27 22:06:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 import os
@@ -11,8 +13,11 @@ password = os.environ.get("Password")
 class Walcart():
 
     def setUp(self):
-        self.driver = webdriver.Chrome("U:\chromedriver.exe")
+        ###old chromedriver path
+        #self.driver = webdriver.Chrome("U:\chromedriver.exe")
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.driver.maximize_window()
+        ### for a specific window size
         #self.driver.set_window_position(0, 0)
         #self.driver.set_window_size(1920, 1080)
         self.driver.get("https://www.walcart.com/")
@@ -24,8 +29,9 @@ class Walcart():
         self.driver.implicitly_wait(2)
         self.driver.find_element(By.ID, "pass").send_keys(password)
         self.driver.find_element(By.CLASS_NAME, "mobile-login-code-button").click()
-        #welcomeMessage = self.driver.find_element(By.CLASS_NAME, "customer-welcome acc-topbar")
-        #assert "Hi, Nafiz" in welcomeMessage
+        welcomeText = self.driver.find_element(By.CLASS_NAME, "acc-btn").text
+        ### Verify if the log in was successful
+        assert "Hi," == welcomeText
 
     def test_order(self):
         time.sleep(5)
@@ -39,12 +45,16 @@ class Walcart():
         self.driver.execute_script("arguments[0].click();", element)
         element2 = self.driver.find_element(By.XPATH, ".//input[@type='radio' and @value='cashondelivery']")
         self.driver.execute_script("arguments[0].click();", element2)
+        ### verify if the checkout button is clickable
         assert self.driver.find_element(By.CLASS_NAME, "checkout").is_displayed()
 
     def test_logout(self):
         self.driver.find_element(By.CLASS_NAME, "acc-btn").click()
         self.driver.find_element(By.CLASS_NAME, "mbi-exit").click()
-
+        self.driver.implicitly_wait(4)
+        loginText = self.driver.find_element(By.CLASS_NAME, "phoneview-user").text
+        ### Verify if the log out was successful
+        assert "Log In" == loginText
 
     def shutdown(self):
         self.driver.close()
@@ -53,6 +63,6 @@ class Walcart():
 x = Walcart()
 x.setUp()
 x.test_login()
-x.test_order()
 x.test_logout()
+x.test_order()
 x.shutdown()
